865e36b4196e15e0d2693ad0c77402c4c85bc630
Benjamin Blanchard <56205597+BenjaminB31@users.noreply.github.com>
2020-03-27 22:16:17 +0100

    Add files via upload

--- yajeuxdete.py
+++ yajeuxdete.py
@@ -1,7 +1,7 @@
 a = int(input( " Entrez une date : "))
 
-if(a>=1896):
-	if(a!=1916 and a!=1940 and a!= 1944):
+if(a!=1916 and a!=1940 and a!= 1944):
+	if(a>=1896):
 		if(a%4==0 and a%100!=0 or a%400==0):
 			print("True")
 		else:
--- yajeuxdhivert.py
+++ yajeuxdhivert.py
@@ -1,7 +1,7 @@
 a = int(input( " Entrez une date : "))
 
-if(a>=1994):
-    if(a!=1916 and a!=1940 and a!= 1944):
+if(a!=1916 and a!=1940 and a!= 1944):
+    if(a>=1924):
         if(a%4==2 and a%100!=2 or a%400==2):
             print("True")
         else:
