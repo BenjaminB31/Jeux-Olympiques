3d51f85b557363db104dd686aba10105c7118624
Benjamin Blanchard <56205597+BenjaminB31@users.noreply.github.com>
2020-03-27 22:16:39 +0100

    Update yajeux.py

--- yajeux.py
+++ yajeux.py
@@ -1,7 +1,7 @@
 a = int(input( " Entrez une date : "))
 
 if((a!=1916 and a!=1940 and a!= 1944) and (a>=1896) and (a%4==0 and a%100!=0 or a%400==0)):
-    print("True1")
+    print("True")
 elif((a!=1916 and a!=1940 and a!= 1944) and (a>=1924) and (a%4==2 and a%100!=2 or a%400==2)):
     print("True")
 else:
